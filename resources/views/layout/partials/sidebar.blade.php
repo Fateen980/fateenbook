dcc95f9d9570c963bf23b15ebe5aea6983d72bdc
vagrant <vagrant@homestead>
2016-08-24 17:33:15 +0000

    Login Design

--- resources/views/layout/partials/sidebar.blade.php
+++ resources/views/layout/partials/sidebar.blade.php
@@ -5,29 +5,11 @@
     {!! Form::open(array('url' => 'login')) !!}
 	  <button type="submit" class="flat-btn">Sign In, Man!</button>
     {!! Form::close() !!}
+
+
     <h1><img src="img/logo.png" class="img-responsive" width="200px" height="150px"></h1>
  
-    <div id="emailsignup" class="email-signup animated">
-      <span class="email-signup-head">Feeder is awsome!</span>
-      <p>Please enter your e-mail address .</p>
  
-      {!! Form::open(array('url' => 'signup')) !!}
-        <input  type="email"  name="email" class="email-input" placeholder="you@me.com" tabindex="1">
-        <button type="submit" class="flat-btn">Sign Me Up, Tony!</button>
-         @if (count($errors) > 0)
-         <br>
-    <div class="alert alert-danger">
-       
-            @foreach ($errors->all() as $error)
-               {{ $error }}
-            @endforeach
-        
-    </div>
-    @endif
-      {!! Form::close() !!}
-
-      
-    </div><!-- @end .email-signup -->
    
   </div><!-- @end #wrapper -->
 </div>
